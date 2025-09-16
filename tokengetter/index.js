80b0b1eea0542f74321351e7f01c78b000c6df6f
Devin Little <devinlittlee@gmail.com>
2025-09-15 20:13:52 -0400

    fix(gradegetter): fixed the dockerfile and yeah it works now! YIPPIE

--- tokengetter/index.js
+++ tokengetter/index.js
@@ -3,11 +3,22 @@ const puppeteer = require('puppeteer');
 (async () => {
   const args = process.argv;
   const browser = await puppeteer.launch({
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH, 
-    headless: true,
-    args: ["--incognito", "--no-sandbox", "--disable-setuid-sandbox"]
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    headless: 'new',
+    args: ['--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-blink-features=AutomationControlled',
+      '--window-size=1280,720']
   });
+
   const page = await browser.newPage();
+
+  await page.evaluateOnNewDocument(() => {
+    Object.defineProperty(navigator, 'webdriver', { get: () => false });
+  });
+  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36');
 
   // login
   await page.goto('https://essexnorthshore.schoology.com/');
@@ -37,4 +48,3 @@ const puppeteer = require('puppeteer');
 
   browser.close();
 })();
-
